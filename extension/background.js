ec36252116c70d9eaa89869dd4e62dece7a43f90
Abhishek7Tech <abhishektab45@gmail.com>
2024-02-22 17:21:01 +0530

    implemented observerMutation functionality

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,22 @@
 try {
-  let i = 0;
+  let url;
  
 
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if(url === tab.url) return;
     if (
-      i < 1 &&
       tab.url &&
       tab.url.includes("youtube.com/watch") &&
       tab.status === "complete"
-    ) {
-      setTimeout(() => {
-        chrome.tabs.sendMessage(tabId, {
-          type: "RATE",
-          url: tab.url,
-        });
-      }, 5000);
-      i = i + 1;
-      console.log("SENT RATEING REQUEST", tab.url, i);
-      return;
+      ) {
+        setTimeout(() => {
+          chrome.tabs.sendMessage(tabId, {
+            type: "RATE",
+            url: tab.url,
+          });
+        }, 8000);
+        url = tab.url;
+        return;
     }
   });
 } catch (error) {
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -36,18 +36,7 @@ const getRatings = async (content) => {
   }
 };
 
-const observer = new MutationObserver((mutations) =>
-  mutations.map((item) => {
-    const target = item.target;
-    if ("href" in target) {
-      const link = target.href.slice(23);
-      if (link.includes("/watch") && !updatedContentArray.includes(link)) {
-        updatedContentArray.push(link);
-        console.log("CONTENT-UPDATED", updatedContentArray);
-      }
-    }
-  })
-);
+
 
 const setContentRatings = (rating) => {
   const html = `<div id="content-overlay" style="border: 1px solid red; width: fit-content; z-index: 100; position: absolute; margin:4px">
@@ -70,22 +59,40 @@ const setVideoRating = (rating) => {
 const setOverLay = (ratings, videoURL) => {
   ratings?.map((urls) => {
     const anchorEle = document.querySelector(`a[href='${urls.url}']`);
-    anchorEle.insertAdjacentHTML(
-      "afterbegin",
-      setContentRatings(urls.rating)
-    );
-
-    if (urls.url === videoURL) {
-      console.log("Video url", urls.url, urls.rating);
-      const videoEle = document.querySelector("#movie_player");
-      videoEle.insertAdjacentHTML(
+    if (anchorEle && !anchorEle.querySelector("#content-overlay")) {
+      anchorEle.insertAdjacentHTML(
         "afterbegin",
-        setVideoRating(urls.rating)
+        setContentRatings(urls.rating)
       );
     }
 
+    if (videoURL && urls.url === videoURL) {
+      console.log("Video url", urls.url, urls.rating);
+      const videoEle = document.querySelector("#movie_player");
+      videoEle.insertAdjacentHTML("afterbegin", setVideoRating(urls.rating));
+    }
   });
-}
+};
+
+const observer = new MutationObserver(async (mutations) => {
+  setTimeout(async () => {
+    mutations.forEach((item) => {
+      const target = item.target;
+      if ("href" in target) {
+        const link = target.href.slice(23);
+        console.log("link", link);
+        if (link.includes("/watch") && !updatedContentArray.includes(link)) {
+          updatedContentArray.push(link);
+        }
+      }
+    });
+    if (updatedContentArray.length) {
+      const ratings = await getRatings(updatedContentArray);
+      setOverLay(ratings, "");
+      updatedContentArray = [];
+    }
+  }, 8000);
+});
 
 (async () => {
   try {
@@ -98,16 +105,16 @@ const setOverLay = (ratings, videoURL) => {
           const ratings = await getRatings(contentArray);
           const videoURL = req.url.slice(23);
           setOverLay(ratings, videoURL);
-          observer.observe(
-            document.querySelector(
-              '.style-scope[section-identifier="sid-wn-chips"]'
-            )?.children[2],
-            {
-              attributes: true,
-              subtree: true,
-              childList: true,
-            }
+          const recommendationsEle = document.querySelector(
+            '.style-scope[section-identifier="sid-wn-chips"]'
           );
+
+          const content = recommendationsEle.querySelector("#contents");
+          observer.observe(content, {
+            attributes: true,
+            subtree: true,
+            childList: true,
+          });
         }
       } else {
         console.log("NOT FOUND");
@@ -116,8 +123,6 @@ const setOverLay = (ratings, videoURL) => {
   } catch (error) {
     console.log("CS_ERROR", error);
   }
-
-  return;
 })();
 
 // document.querySelector("a[href='/watch?v=MPhz_EqctdA']");
