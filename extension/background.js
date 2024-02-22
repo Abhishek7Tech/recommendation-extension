4f07376f5bd4e2df57a08f8afb4b9bd70e9251e6
Abhishek7Tech <abhishektab45@gmail.com>
2024-02-22 18:24:19 +0530

    updates

--- extension/background.js
+++ extension/background.js
@@ -1,22 +1,30 @@
 try {
   let url;
- 
 
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if(url === tab.url) return;
+    console.log("STARTED");
+    if (url === tab.url) return;
+    console.log("STARTED-2");
+
     if (
       tab.url &&
       tab.url.includes("youtube.com/watch") &&
       tab.status === "complete"
-      ) {
-        setTimeout(() => {
-          chrome.tabs.sendMessage(tabId, {
-            type: "RATE",
-            url: tab.url,
-          });
-        }, 8000);
-        url = tab.url;
-        return;
+    ) {
+      console.log("STARTED-3");
+
+      setTimeout(() => {
+        chrome.tabs.sendMessage(tabId, {
+          type: "RATE",
+          url: tab.url,
+        });
+      }, 8000);
+      console.log("STARTED-4");
+
+      url = tab.url;
+      console.log("STARTED-5",tab.url);
+
+      return;
     }
   });
 } catch (error) {
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,81 +1,79 @@
 let contentArray = [];
 let updatedContentArray = [];
 
-import rateRecommendations from "./utils/rateRecommendations";
-import getRatings from "./utils/getRatings";
-import setOverLay from "./utils/setOverLay";
-// const rateRecommendations = () => {
-//   contentArray.splice(0, contentArray.length);
-//   document
-//     .querySelectorAll(
-//       "a"
-//       // ).forEach((item) => console.log("ITEM__2", item.getAttribute("id") === "thumbnail" && item.getAttribute("href")?.includes("/watch") && contentArray.includes(item.getAttribute("href")) ? "" : contentArray.push(item.getAttribute("href"))));
-//     )
-//     .forEach(
-//       (item) =>
-//         item.getAttribute("id") === "thumbnail" &&
-//         item.getAttribute("href")?.includes("/watch") &&
-//         !contentArray.includes(item.getAttribute("href")) &&
-//         contentArray.push(item.getAttribute("href"))
-//     );
-// };
 
-// const getRatings = async (content) => {
-//   if (!content.length) return;
+const rateRecommendations = () => {
+  contentArray.splice(0, contentArray.length);
+  document
+    .querySelectorAll(
+      "a"
+      // ).forEach((item) => console.log("ITEM__2", item.getAttribute("id") === "thumbnail" && item.getAttribute("href")?.includes("/watch") && contentArray.includes(item.getAttribute("href")) ? "" : contentArray.push(item.getAttribute("href"))));
+    )
+    .forEach(
+      (item) =>
+        item.getAttribute("id") === "thumbnail" &&
+        item.getAttribute("href")?.includes("/watch") &&
+        !contentArray.includes(item.getAttribute("href")) &&
+        contentArray.push(item.getAttribute("href"))
+    );
+};
 
-//   const res = await fetch("http://localhost:7000/", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
+const getRatings = async (content) => {
+  if (!content.length) return;
 
-//     body: JSON.stringify({ urls: content }),
-//   });
+  const res = await fetch("http://localhost:7000/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
 
-//   if (res.ok) {
-//     const ratings = await res.json();
-//     return ratings;
-//   }
-// };
+    body: JSON.stringify({ urls: content }),
+  });
+
+  if (res.ok) {
+    const ratings = await res.json();
+    return ratings;
+  }
+};
 
 
 
-// const setContentRatings = (rating) => {
-//   const html = `<div id="content-overlay" style="border: 1px solid red; width: fit-content; z-index: 100; position: absolute; margin:4px">
-//   <div style="background-color: orange;">
-//       <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black">${rating}</h2>
-//   </div>
-// </div>`;
-//   return html;
-// };
+const setContentRatings = (rating) => {
+  const html = `<div id="content-overlay" style="border: 1px solid red; width: fit-content; z-index: 100; position: absolute; margin:4px">
+  <div style="background-color: orange;">
+      <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black">${rating}</h2>
+  </div>
+</div>`;
+  return html;
+};
 
-// const setVideoRating = (rating) => {
-//   const html = `<div id="video-overlay" style="border: 1px solid red; width: 50px; height: 40px; z-index: 100; position: absolute; margin:8px">
-//   <div style="background-color: orange; height:40px;">
-//       <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black; font-size: 24px">${rating}</h2>
-//   </div>
-// </div>`;
-//   return html;
-// };
+const setVideoRating = (rating) => {
+  const html = `<div id="video-overlay" style="border: 1px solid red; width: 50px; height: 40px; z-index: 100; position: absolute; margin:8px">
+  <div style="background-color: orange; height:40px;">
+      <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black; font-size: 24px">${rating}</h2>
+  </div>
+</div>`;
+  return html;
+};
 
-// const setOverLay = (ratings, videoURL) => {
-//   ratings?.map((urls) => {
-//     const anchorEle = document.querySelector(`a[href='${urls.url}']`);
-//     if (anchorEle && !anchorEle.querySelector("#content-overlay")) {
-//       anchorEle.insertAdjacentHTML(
-//         "afterbegin",
-//         setContentRatings(urls.rating)
-//       );
-//     }
+const setOverLay = (ratings, videoURL) => {
+  ratings?.map((urls) => {
+    const anchorEle = document.querySelector(`a[href='${urls.url}']`);
+    if (anchorEle && !anchorEle.querySelector("#content-overlay")) {
+      anchorEle.insertAdjacentHTML(
+        "afterbegin",
+        setContentRatings(urls.rating)
+      );
+    }
 
-//     if (videoURL && urls.url === videoURL) {
-//       console.log("Video url", urls.url, urls.rating);
-//       const videoEle = document.querySelector("#movie_player");
-//       videoEle.insertAdjacentHTML("afterbegin", setVideoRating(urls.rating));
-//     }
-//   });
-// };
+    if (videoURL && urls.url === videoURL) {
+      console.log("Video url", urls.url, urls.rating);
+      const videoEle = document.querySelector("#movie_player");
+      videoEle.insertAdjacentHTML("afterbegin", setVideoRating(urls.rating));
+    }
+  });
+};
 
 const observer = new MutationObserver(async (mutations) => {
   setTimeout(async () => {
