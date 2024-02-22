ce4a61bcb8518d13c082db5b58abd37442a4f4f3
Abhishek7Tech <abhishektab45@gmail.com>
2024-02-22 12:45:37 +0530

    implemented overlay function

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,4 +1,5 @@
 let contentArray = [];
+let updatedContentArray = [];
 
 const rateRecommendations = () => {
   contentArray.splice(0, contentArray.length);
@@ -35,8 +36,21 @@ const getRatings = async (content) => {
   }
 };
 
+const observer = new MutationObserver((mutations) =>
+  mutations.map((item) => {
+    const target = item.target;
+    if ("href" in target) {
+      const link = target.href.slice(23);
+      if (link.includes("/watch") && !updatedContentArray.includes(link)) {
+        updatedContentArray.push(link);
+        console.log("CONTENT-UPDATED", updatedContentArray);
+      }
+    }
+  })
+);
+
 const setContentRatings = (rating) => {
-  const html = `<div style="border: 1px solid red; width: fit-content; z-index: 100; position: absolute; margin:4px">
+  const html = `<div id="content-overlay" style="border: 1px solid red; width: fit-content; z-index: 100; position: absolute; margin:4px">
   <div style="background-color: orange;">
       <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black">${rating}</h2>
   </div>
@@ -45,7 +59,7 @@ const setContentRatings = (rating) => {
 };
 
 const setVideoRating = (rating) => {
-  const html = `<div style="border: 1px solid red; width: 50px; height: 40px; z-index: 100; position: absolute; margin:8px">
+  const html = `<div id="video-overlay" style="border: 1px solid red; width: 50px; height: 40px; z-index: 100; position: absolute; margin:8px">
   <div style="background-color: orange; height:40px;">
       <h2 style="text-align: center; vertical-align: middle; margin: 0px; padding: 2px; color: black; font-size: 24px">${rating}</h2>
   </div>
@@ -53,8 +67,27 @@ const setVideoRating = (rating) => {
   return html;
 };
 
+const setOverLay = (ratings, videoURL) => {
+  ratings?.map((urls) => {
+    const anchorEle = document.querySelector(`a[href='${urls.url}']`);
+    anchorEle.insertAdjacentHTML(
+      "afterbegin",
+      setContentRatings(urls.rating)
+    );
+
+    if (urls.url === videoURL) {
+      console.log("Video url", urls.url, urls.rating);
+      const videoEle = document.querySelector("#movie_player");
+      videoEle.insertAdjacentHTML(
+        "afterbegin",
+        setVideoRating(urls.rating)
+      );
+    }
+
+  });
+}
+
 (async () => {
-  console.log("CONTENT ARRAY", contentArray);
   try {
     chrome.runtime.onMessage.addListener(async (req, sender, res) => {
       console.log("REQ", req);
@@ -64,35 +97,17 @@ const setVideoRating = (rating) => {
         if (contentArray.length) {
           const ratings = await getRatings(contentArray);
           const videoURL = req.url.slice(23);
-          ratings?.map((urls) => {
-            const anchorEle = document.querySelector(`a[href='${urls.url}']`);
-            anchorEle.insertAdjacentHTML(
-              "afterbegin",
-              setContentRatings(urls.rating)
-            );
-
-            if (urls.url === videoURL) {
-              console.log("Video url", urls.url, urls.rating);
-              const videoEle = document.querySelector("#movie_player");
-              videoEle.insertAdjacentHTML(
-                "afterbegin",
-                setVideoRating(urls.rating)
-              );
+          setOverLay(ratings, videoURL);
+          observer.observe(
+            document.querySelector(
+              '.style-scope[section-identifier="sid-wn-chips"]'
+            )?.children[2],
+            {
+              attributes: true,
+              subtree: true,
+              childList: true,
             }
-
-            const observer = new MutationObserver((mutations) => console.log("CHANGES ARE LIVE", mutations));
-
-            observer.observe(
-              //  document.querySelector('ytd-item-section-renderer .style-scope.ytd-watch-next-secondary-results-renderer'),
-
-              // document.querySelector('body #related.style-scope.ytd-watch-flexy'),
-              {
-                subtree: true,
-                childList: true
-              }
-            );
-            // setRatings(urls.rating, anchorEle);
-          });
+          );
         }
       } else {
         console.log("NOT FOUND");
@@ -102,7 +117,6 @@ const setVideoRating = (rating) => {
     console.log("CS_ERROR", error);
   }
 
- 
   return;
 })();
 
